6e6fa84ceb1a105e03a0e739c4f3c03077764df4
Arun <arunarofficial00@gmail.com>
2023-11-11 14:30:41 +0530

    Array tostring

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Demo{
     public static void main(String[] args){
         int age=45;
@@ -6,13 +8,29 @@ public class Demo{
         //datatype
         byte Age=30;
         System.out.println(Age);
-        
+        //when using long use l/L as suffix to the number
 
         String message=" Hello word" + " !!";
         System.out.println(message.endsWith("!!"));
         System.out.println(message.replace("!","*"));
         System.out.println(message.trim());
 
-        //when using long use l/L as suffix to the number
+
+        // Arrays
+
+        int[] numbers =new int[5];
+        numbers[0]=1;
+        numbers[1]=2;
+        numbers[2]=3;
+        numbers[3]=4;
+        
+
+        int[] num={1,2,3,4};
+        System.out.println(Arrays.toString(num));
+
+        System.out.println(numbers.length);
+
+        System.out.println(Arrays.toString(numbers));
+        
     }
 }
