88959f7796af57a7a00a6e6867bcfd22dd74d8ec
Arun <arunarofficial00@gmail.com>
2023-11-11 14:13:26 +0530

    trim

--- Java/Hello.java
+++ Java/Hello.java
@@ -1,16 +1,17 @@
 public class Hello{
     public static void main(String[] args){
         int age=45;
-        System.out.println("Hello");
+        System.out.println(" Hello");
         System.out.println(age);
         //datatype
         byte Age=30;
         System.out.println(Age);
         
 
-        String message="Hello word" + " !!";
+        String message=" Hello word" + " !!";
         System.out.println(message.endsWith("!!"));
         System.out.println(message.replace("!","*"));
+        System.out.println(message.trim());
 
         //when using long use l/L as suffix to the number
     }
