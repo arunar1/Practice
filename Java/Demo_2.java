8735ecfe6ad51a4efdbb569aa1d01f65f607982e
Arun <arunarofficial00@gmail.com>
2023-11-12 14:06:37 +0530

    Commit

---
+++ Java/Demo_2.java
@@ -0,0 +1,8 @@
+public class Demo_2 {
+    public static void main(String[] args){
+        int temperature =22;
+        boolean iswarm=temperature>20 && temperature<30;
+        System.out.println(iswarm);
+    }
+    
+}
