31c3d9b61438e1872739e94e9a495213d7969454
Arun <arunarofficial00@gmail.com>
2023-11-13 21:45:05 +0530

    commit

--- Java/Calc.java
+++ Java/Calc.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 
 public class Calc {
     public static void main(String[] args){
-        Scanner scanner=new Scanner(System.in);
 
-        
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the operation :");
+        System.out.println("+: Addition");
+        System.out.println("-: Subtraction");
+        System.out.println("/: Dicision");
+        System.out.println("*: Multiplication");
+
+
     }
     
 }
