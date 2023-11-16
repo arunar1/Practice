4b8cca709e93911c5b7e8c3ab796138231612f92
Arun <arunarofficial00@gmail.com>
2023-11-16 17:31:04 +0530

    commit

--- Java/Class.java
+++ Java/Class.java
@@ -1,8 +1,15 @@
+
 public class Class {
     int a=10;
+    
     public static void main(String[] args) {
         Class obj=new Class();
         System.out.println(obj.a);
+        // System.out.println(args[0]);
+        
     }
     
+    
 }
+
+
---
+++ Java/constructor.java
@@ -0,0 +1,26 @@
+public class constructor {
+    String name;
+    int rno;
+    constructor(){
+        name="abin";
+        rno=67;
+    }
+    // parameterized constructor
+
+    constructor(String str, int rn){
+        name=str;
+        rno=rn;
+    }
+    public static void main(String[] args) {
+
+        constructor obj1=new constructor();
+        System.out.println(obj1.name);
+        System.out.println(obj1.rno);
+
+        constructor obj2 =new constructor("manu",45);
+        System.out.println(obj2.name);
+        System.out.println(obj2.rno);
+        
+    }
+    
+}
