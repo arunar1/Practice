4ee4879f045914e0112a7107c40c4aa7bd8eb971
Arun <arunarofficial00@gmail.com>
2023-11-18 15:57:40 +0530

    inheritance

---
+++ Java/Classchild.java
@@ -0,0 +1,27 @@
+
+
+class A{
+    int a =5;
+    A(){
+        System.out.println("Welcome to parent class");
+    }
+
+}
+class B extends A{ 
+    B(){
+        System.out.println("Child class");
+    }
+
+}
+
+
+
+public class Classchild {
+    public static void main(String[] args) {
+        B obj1=new B();
+        System.out.println(obj1.a);
+
+        
+    }
+    
+}
