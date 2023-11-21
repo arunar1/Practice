547d453bf9956cb3d2f9c72293e8f9d538490be8
Arun <arunarofficial00@gmail.com>
2023-11-21 22:10:10 +0530

    super keyword

--- Java/Pro.java
+++ Java/Pro.java
@@ -1,6 +1,30 @@
+
+
+
+class A{
+    int a=20;
+    void display(){
+        System.out.println("Parent");
+    }
+
+}
+class B extends A{
+    int a=40;
+    void display(){
+        System.out.println("Child");
+        System.out.println(a);
+        System.out.println(super.a);
+    }
+}
+
+
+
 public class Pro {
     public static void main(String[] args) {
         System.out.println("welcome to Programing");
+        B obj1 =new B();
+        obj1.display();
+
     }
     
 }
