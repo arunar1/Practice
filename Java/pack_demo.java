78c9395b418b3aede065c717389c222de55ab986
Arun <arunarofficial00@gmail.com>
2023-12-13 13:43:15 +0530

    into to multithreading

--- Java/pack_demo.java
+++ Java/pack_demo.java
@@ -1,5 +1,9 @@
-import pack.*;
+import pack.package_demo;
 public class pack_demo {
+    public static void main(String[] args) {
+        package_demo obj1 = new package_demo();
+        obj1.show();
+    }
     
     
 }
