2283b841a07ae37a4a331bf5ad043713c75af679
Arun <arunarofficial00@gmail.com>
2023-12-20 22:55:16 +0530

    getname getpriority

--- Java/method_thread_demo.java
+++ Java/method_thread_demo.java
@@ -11,6 +11,8 @@ class method_thred_demo{
     public static void main(String[] args) {
         th t1=new th();
         System.out.println("ID :"+t1.getId());
+        System.out.println("Name :"+t1.getName());
+        System.out.println("Priority :"+t1.getPriority());
         t1.start();
         
     }
