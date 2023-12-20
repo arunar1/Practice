ea051a11f12505fb2aa8dc6e5f17cb1ad5368b1e
Arun <arunarofficial00@gmail.com>
2023-12-20 22:53:22 +0530

    getid

--- Java/method_thread_demo.java
+++ Java/method_thread_demo.java
@@ -10,6 +10,7 @@ class th  extends Thread{
 class method_thred_demo{
     public static void main(String[] args) {
         th t1=new th();
+        System.out.println("ID :"+t1.getId());
         t1.start();
         
     }
