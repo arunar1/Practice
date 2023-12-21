c07f665d5385419e06558eff84ba5eaa1b43805c
Arun <arunarofficial00@gmail.com>
2023-12-21 13:04:52 +0530

    join used to on thread complete then thread 2 starts

--- Java/method_thread_demo.java
+++ Java/method_thread_demo.java
@@ -16,12 +16,25 @@ class th  extends Thread{
 class method_thred_demo{
     public static void main(String[] args) {
         th t1=new th();
+        th t2=new th();
         System.out.println("ID :"+t1.getId());
         System.out.println("Name :"+t1.getName());
         System.out.println("Priority :"+t1.getPriority());
         t1.setName("welcome");
         System.out.println("Name (after changing) :"+t1.getName());
+        System.out.println("Priority of thread :"+t1.getPriority());
+        t1.setPriority(1);
+        System.out.println("Priority of thread (After changing):"+t1.getPriority());
+
         t1.start();
+
+        try {
+            t1.join();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        t2.start();
         
     }
 }
