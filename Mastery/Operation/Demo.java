9708190a35e39d86b86415916b2d7c4fa60d8e36
Arun <arunarofficial00@gmail.com>
2023-12-28 22:02:15 +0530

    basic ops

---
+++ Mastery/Operation/Demo.java
@@ -0,0 +1,10 @@
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Basic operation");
+        int firstNum=3;
+        int secondNum=4;
+        System.out.println("The sum is "+firstNum+secondNum);
+    }
+    
+}
