785a8a8e487091b4bdbe83bc75697e990802d944
Arun <arunarofficial00@gmail.com>
2023-12-28 21:15:19 +0530

    commit

---
+++ Mastery/Variable/demo_1.java
@@ -0,0 +1,27 @@
+class demo_1{
+    public static void main(String[] args) {
+        String myName="kasim";
+        String myDog="tomy";
+
+        // Byte for small numbers  less then 128
+        byte age=127;
+
+        //short 2x byte 
+        short myShort=5000;
+
+        //float
+        float myFloat=1.5f;
+
+        //double
+
+        double myDouble=1.7;
+
+
+
+
+         
+        System.out.println(myName +" has a dog " + myDog+ " age of " +myShort);
+        System.out.println(myDouble);
+    }
+
+}
--- Mastery/demo_1.java
+++
@@ -1,9 +0,0 @@
-class demo_1{
-    public static void main(String[] args) {
-        String myName="kasim";
-        String myDog="tomy";
-
-        System.out.println(myName +" has a dog " + myDog);
-    }
-
-}
