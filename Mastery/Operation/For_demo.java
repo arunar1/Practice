e4dc7d0e0575b51a4c0ca93e7b67e0982d6cf66e
Arun <arunarofficial00@gmail.com>
2023-12-29 13:46:35 +0530

    commit

---
+++ Mastery/Operation/For_demo.java
@@ -0,0 +1,24 @@
+
+public class For_demo {
+    public static void main(String[] args) {
+        System.out.println("LOOPS");
+
+        // for loop
+        for(int i=1;i<10;i++){
+            if(i%2==0){
+                System.out.println(i + " is a multiple of 2");
+
+            }
+
+        }
+
+        //while loop
+        System.out.println("While loop");
+        int i=1;
+        while(i<5){
+            System.out.println(i);
+            i=i+1;
+        }
+    }
+    
+}
---
+++ Mastery/Operation/Method.java
@@ -0,0 +1,11 @@
+
+public class Method {
+    public static void main(String[] args) {
+        myName();
+         
+    }
+    public static void  myName(){
+        System.out.println("Arun");
+    }
+    
+}
