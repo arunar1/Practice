dabbf2fd3761ac080171ea600542599cef265776
Arun <arunarofficial00@gmail.com>
2023-12-30 22:05:25 +0530

    commit

--- Mastery/Operation/Method.java
+++ Mastery/Operation/Method.java
@@ -1,11 +1,16 @@
 
 public class Method {
     public static void main(String[] args) {
-        myName();
+        myName("Arun");
+        System.out.println(add(2,3));
          
     }
-    public static void  myName(){
-        System.out.println("Arun");
+    public static int add(int a, int b){
+        return a+b;
+
+    }
+    public static void  myName(String str){
+        System.out.println(str);
     }
     
 }
