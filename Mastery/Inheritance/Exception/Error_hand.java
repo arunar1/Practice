5c82c28439b80cc533286d143fa4e67980c727e6
Arun <arunarofficial00@gmail.com>
2024-01-07 23:09:13 +0530

    commit

---
+++ Mastery/Inheritance/Exception/Error_hand.java
@@ -0,0 +1,15 @@
+// import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Error_hand {
+    public static void main(String[] args) {
+        //array two way initialization
+
+        int[] number={1,2,3,4,5,};
+        int demo[]={2,3,4};
+        System.out.println(number[0]);
+        System.out.println(Arrays.toString(demo));
+        // System.out.println(Arrays.asList(number));
+    }
+    
+}
