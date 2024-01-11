8966ae24582eea4d05c3187c82c7bc7e133063a6
Arun <arunarofficial00@gmail.com>
2024-01-11 18:15:21 +0530

    array

---
+++ Mastery/DataStructure/Arrays/demo.java
@@ -0,0 +1,13 @@
+
+public class demo {
+    
+    // arrays are container put item of same type
+
+    public static void main(String[] args) {
+        int[] ar={5,12,153,44};
+        // length used to get the length of the array
+        for(int i=0;i<ar.length;i++){
+            System.out.println(" Element "+ (i+1) +" "+ar[i]);
+        }
+    }
+}
