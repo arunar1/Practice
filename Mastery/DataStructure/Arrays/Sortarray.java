73e17e49db0b37d82fa740f2cfcb7ececb5cdd69
Arun <arunarofficial00@gmail.com>
2024-01-21 23:40:46 +0530

    array sort

---
+++ Mastery/DataStructure/Arrays/Sortarray.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Sortarray {
+    public static void main(String[] args) {
+        int[] arrInt={7,2,3,4,5};
+
+        String[] arrStrings={"arun","manu","anamika"};
+
+        boolean[] arrBools={true,false,true};
+
+        Arrays.sort(arrInt);
+        Arrays.sort(arrStrings);
+
+        System.out.println(Arrays.toString(arrInt));
+
+        System.out.println(Arrays.toString(arrStrings));
+    }
+    
+}
